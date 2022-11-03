c5652c3ac89e495b4f6485aa09538a1c726168ad
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-03 14:41:41 -0700

    test wrote that renders out all players

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,10 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
+const app = require('../lib/app');
 // const request = require('supertest');
 // const app = require('../lib/app');
-const { osuPlayers } = require('../')
+const { osuPlayers } = require('../lib/models/osuModel');
+const { request } = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
@@ -11,8 +13,12 @@ describe('backend-express-template routes', () => {
 
 
 
-  it('/osu should render a list of all osu players', () => {
-    
+  it('/osu should render a list of all osu players', async () => {
+    const res = await request(app).get('/osu');
+    const expected = osuPlayers.map((osu) => {
+      return { id: osu.id, name: osu.name, rank: osu.rank, top_play: osu.top_play, play_style: osu.play_style, country: osu.country };
+    });
+    expect(res.body).toEqual(expected);
   });
 
 
