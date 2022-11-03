9c7451ce0fb88b676168adbb284a8a2ecaeae992
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-03 14:56:56 -0700

    got test to pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,9 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const app = require('../lib/app');
-// const request = require('supertest');
+const request = require('supertest');
 // const app = require('../lib/app');
-const { osuPlayers } = require('../lib/models/osuModel');
-const { request } = require('../lib/app');
+const OsuGame = require('../lib/models/osuModel');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
@@ -15,11 +14,15 @@ describe('backend-express-template routes', () => {
 
   it('/osu should render a list of all osu players', async () => {
     const res = await request(app).get('/osu');
-    const expected = osuPlayers.map((osu) => {
+    const allPlayers = await OsuGame.getAllPlayers();
+    const expected = allPlayers.map((osu) => {
       return { id: osu.id, name: osu.name, rank: osu.rank, top_play: osu.top_play, play_style: osu.play_style, country: osu.country };
     });
     expect(res.body).toEqual(expected);
   });
+
+
+  
 
 
 
